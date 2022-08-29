33a692528d79a99fe0eb2f8ab3bf19b2e35bd007
Geovannalc <linsgeovanna@outlook.com>
2022-08-29 11:52:23 -0300

    Adicionar arquivos de projeto.

---
+++ Meuprimeiroprograma-CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿const string textoolamundo = "Olá mundo, meu primeiro programa c#! Feito por ";
+string nomecompleto;
+int idade;
+
+Console.WriteLine("Qual o seu nome completo?");
+
+nomecompleto = Console.ReadLine().ToString();
+
+Console.WriteLine("Qual a sua idade?");
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(textoolamundo + nomecompleto + " que tem " + idade + " anos ");
